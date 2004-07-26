791d22189e63b8dc1fa89252944d5c4c7f36a391
weiti <weiti@f69e5300-e0df-0310-b503-eafd5b73833c>
2004-07-26 22:47:31 +0000

    Added Function increase_serial($serial)
    This function tries it best to increase a date based serial number
    as "YYYYmmddXX" where XX is an daily counter. With this it is possible to
    reflect 99 Changes per day in the serial number. The function returns the 
    unmodified Serial in case of an failure to avoid problems within Serialnumbering
    and reloading ...

--- dnsZone-Manager-dev/php/lib/dnsmgr.php
+++ dnsZone-Manager-dev/php/lib/dnsmgr.php
@@ -17,5 +17,35 @@ class DNSMGR {
 
     return $result;
   }
+
+  function increase_serial($Serial) {
+    // First strip of last two digits (these are only for daily counters)
+    $serial_day_counter = substr($Serial, -2);
+    $serial_date_part = substr($Serial, 0, (strlen($Serial) - 2) );
+    
+    $cur_date = date("Ymd");
+
+    if ( $serial_day_counter == 99 ) { 
+      // 99 changes at one day ... thats enough!!!
+      // Stay at 99 and wait for next day!
+      return $Serial; 
+    }
+    
+    if ( "$serial_date_part" == "$cur_date" ) {
+      $serial_day_counter++;
+      if ($serial_day_counter <  10 ) {
+        $serial_day_counter = "0".$serial_day_counter;
+      }
+      $Serial = $serial_date_part . $serial_day_counter ;
+    } else if ( $serial_date_part < $cur_date ) {
+      $Serial = $cur_date . "00";
+    }
+    
+    // If the Serial_date_part is bigger than current day
+    // something went wrong and we give back the original Serial without
+    // modification
+    
+    return $Serial;
+  }
 }
 ?>
